5382ecbb8c1383f3dedd777c8450aa19975a6008
rented_panda <anuswantp1998@gmail.com>
2020-08-22 14:53:28 +0530

    reviews updated

--- components/card.js
+++ components/card.js
@@ -23,31 +23,35 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export default function Card(prop) {
-    let divi = prop.side == "l" ? 3 : 2
-    let divider = prop.side == "l" ? 30 : 20
+export default function Card({
+    side,
+    review
+}) {
+    console.log(review)
+    let divi = side == "l" ? 4 : 2
+    let divider = side == "l" ? 40 : 25
     const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / divi) / divider, 1.05]
     const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
     const matches = useMediaQuery('(min-width:600px)');
     const matchesIpad = useMediaQuery('(min-width:1025px)');
     const classes = useStyles()
-    const [props, set] = useSpring(() => ({ xys: [0.5, -1, 1], config: { mass: 15, tension: 700, friction: 50 } }))
+    const [props, set] = useSpring(() => ({ xys: [0.5, -1, 1], config: { mass: 20, tension: 700, friction: 50 } }))
     return (
         <div>
             <Container style={{ padding: 20, width: "100%" }}>
                 <animated.div
                     class="card"
                     onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-                    onMouseLeave={() => set({ xys: [0, 0, 1] })}
+                    onMkeyouseLeave={() => set({ xys: [0, 0, 1] })}
                     style={{ transform: props.xys.interpolate(trans) }}
                 >
                     <div elevation={2} square={true} className={styles.card} >
                         <div style={{padding:"10px"}}>
                             <Typography color="primary" style={{ marginBottom: 10, fontSize: 15 }}>
-                                <span style={{ fontSize: 22 }}>&#8220;</span> Review is an evaluation of a publication, service, or company such as a movie, video game, musical composition, book; a piece of hardware like a car, home appliance, or computer <span style={{ fontSize: 22 }}>&#8221;</span>
+                                <span style={{ fontSize: 22 }}>&#8220;</span>{review.review}<span style={{ fontSize: 22 }}>&#8221;</span>
                             </Typography>
                             <Typography variant="h6" color="primary" style={{ textAlign: "end" }}>
-                                - Aditya Agarwal,DevOps Engineer
+                                {`- ${review.name}`}
                             </Typography>
                         </div>
                     </div>
--- components/work.js
+++ components/work.js
@@ -3,6 +3,22 @@ import { Container, Typography, Grid } from "@material-ui/core"
 import styles from "./styles.module.css"
 import Card from './card'
 export default function work(props) {
+    let reviewData=[{
+        name:"Abhradip Ghosh,Tech Lead at CBNITS",
+        review:"Anuswant is a very good team man. We have been working closely together for the last few months and the main quality that makes him different than others is his ability to learn and adopt new technologies very quickly. He is very curious and passionate about new trends also. He has the potential of playing a key role in the project delivery and management cycle due to his sharp and rational thought process. I would love to continue work with him in the near future and I think that he could be a valuable asset in any development team having a solid determination and mindset."
+        ,side:"l",
+        },
+        {
+            name:"Manoj Kumar,Lead Developer at TCS",
+            review:"Anuswant is a nice person with good skills",
+            side:"r"
+        },
+        {
+            name:"Aditya Agarwal, Senior DevOps Engineer & SRE Professional at Genpact",
+            review:"Anuswant is a very fast learner and focused developer, working with him was really a good experience.",
+            side:"l"
+        },
+    ]
     return (
         <div>
             <Container maxWidth="xl" style={{ height:"auto" }} className={styles.work} >
@@ -12,7 +28,13 @@ export default function work(props) {
                     </Typography>
                     <div style={{ marginLeft: "15%" }}>
                         <Grid container spacing={3}>
-                            <Grid item xs={12} lg={4} md={4}>
+                            {
+                                reviewData.map((data,key)=><Grid item xs={12} lg={data.side=='r'?4:6} md={data.side=='r'?4:6} key={key}>
+                                        <Card side={data.side} review={data} />
+                                    </Grid>
+                                )
+                            }
+                            {/* <Grid item xs={12} lg={4} md={4}>
                                     <Card side="l" />
                             </Grid>
                             <Grid item xs={12} lg={6} md={6}>
@@ -23,7 +45,7 @@ export default function work(props) {
                             </Grid>
                             <Grid item xs={12} lg={4} md={4}>
                                     <Card side="l"/>
-                            </Grid>
+                            </Grid> */}
                         </Grid>
                     </div>
                 </div>
